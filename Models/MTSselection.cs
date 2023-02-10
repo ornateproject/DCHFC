5d94cc65a5d4dda2e57be8fb83dcbfc92031551f
ronyhooke <deveshjha9546@gmail.com>
2023-02-10 13:27:25 +0530

    devesh commit

--- Models/MTSselection.cs
+++ Models/MTSselection.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ssc.Models
 {
@@ -8,8 +9,8 @@ namespace ssc.Models
 
 
         //[StringLength(100, ErrorMessage = "Enter a valid name")]
-            [Required(ErrorMessage = "*")]
-           public string? post_name{ get; set; }
+        [Required(ErrorMessage = "*")]
+        public string? post_name { get; set; }
 
 
         [Required(ErrorMessage = "*")]
@@ -51,6 +52,113 @@ namespace ssc.Models
 
         [Required(ErrorMessage = "*")]
         public string? ur_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public string? obc_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public string? sc_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public string? st_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public string? ews_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public string? total_type { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        //[Required]
+        [RegularExpression("^[0-9]{1,12}$", ErrorMessage = "Enter Number")]
+        public string? vh_type { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        //[Required]
+        [RegularExpression("^[0-9]{1,12}$", ErrorMessage = "Enter Number")]
+        public string? hh_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        //[Required]
+        [RegularExpression("^[0-9]{1,12}$", ErrorMessage = "Enter Number")]
+        public string? oh_type { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        //[Required]
+        [RegularExpression("^[0-9]{1,12}$", ErrorMessage = "Enter Number")]
+        public string? other_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        //[Required]
+        [RegularExpression("^[0-9]{1,12}$", ErrorMessage = "Enter Number")]
+        public string? vac_type { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        //[Required]
+        [RegularExpression("^[0-9]{1,12}$", ErrorMessage = "Enter Number")]
+        public string? num_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public string? prob_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public string? essen_type { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        public string? desirable_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public string? relaxation_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public string? rule_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public string? column_type { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string? order_type { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string? enclose_type { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string? subsequent_type { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string? worked_type { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string? complied_type { get; set; }
+
+
+
+        [Required(ErrorMessage = "*")]
+        //[Required]
+        [RegularExpression("^[0-9]{1,12}$", ErrorMessage = "Enter Number")]
+        public string? office_type { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        //[Required]
+        [RegularExpression("^[0-9]{1,12}$", ErrorMessage = "Enter Number")]
+        public string? deptt_type { get; set; }
 
 
 
