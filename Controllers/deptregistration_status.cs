03a4ce2f901af381547eeacde700d2ec0a640f71
Neha <Neha@LAPTOP-LD47TCI5>
2023-02-14 15:28:18 +0530

    neha

---
+++ Controllers/deptregistration_status.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ssc.Controllers
+{
+    public class deptregistration_status : Controller
+    {
+        public IActionResult deptstatus()
+        {
+            return View();
+        }
+    }
+}
