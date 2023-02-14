c1daf7528d70826e380d94a81b9068941937a297
jhaabhishek8076 <jhaabhishek8076@gmail.com>
2023-02-14 17:40:50 +0530

    abhishek

---
+++ Controllers/US2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ssc.Controllers
+{
+    public class US2Controller : Controller
+    {
+        public IActionResult us2()
+        {
+            return View();
+        }
+    }
+}
