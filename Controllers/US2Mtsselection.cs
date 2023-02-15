1f5f2dd3c7a7a3f0e57880c214fce0edada0ef30
ronyhooke <deveshjha9546@gmail.com>
2023-02-15 16:54:07 +0530

    devesh

---
+++ Controllers/US2Mtsselection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ssc.Controllers
+{
+    public class US2Mtsselection : Controller
+    {
+        public IActionResult us2mts()
+        {
+            return View();
+        }
+    }
+}
