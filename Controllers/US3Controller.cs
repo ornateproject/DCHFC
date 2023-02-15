dd22217c08ff6de40812605a4a752823ea42835f
Neha <Neha@LAPTOP-LD47TCI5>
2023-02-15 12:36:13 +0530

    neha

---
+++ Controllers/US3Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ssc.Models;
+using ssc.repository;
+
+namespace ssc.Controllers
+{
+    public class US3Controller : Controller
+    {
+        private readonly us3MTSrepo _us3repo;
+        public US3Controller(IConfiguration configuration)
+        {
+            _us3repo = new us3MTSrepo(configuration);
+
+
+        }
+
+
+
+        [HttpGet]
+        public IActionResult us3()
+
+
+
+        {
+            var usdata = _us3repo.get_MTSdetails();
+            List<MTSselection> dept = new List<MTSselection>();
+            dept = JsonConvert.DeserializeObject<List<MTSselection>>(usdata);
+            return View(dept);
+        }
+    }
+}
---
+++ repository/us3MTSrepo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace ssc.repository
+{
+    public class us3MTSrepo
+    {
+        private string connectionString;
+
+        public us3MTSrepo(IConfiguration configuration)
+        {
+            connectionString = configuration.GetValue<string>("DBInfo:ConnectionString");
+
+        }
+        public string get_MTSdetails()
+        {
+
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("[sscpost].[Get_MTSdata]", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    con.Open();
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    sda.Fill(dt);
+                    con.Close();
+                }
+            }
+            return JsonConvert.SerializeObject(dt);
+
+        }
+    }
+}
+
