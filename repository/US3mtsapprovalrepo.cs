05702d7069f3b235d5de0bf12886aead7fb38e50
Neha <Neha@LAPTOP-LD47TCI5>
2023-02-15 17:33:41 +0530

    neha

---
+++ repository/US3mtsapprovalrepo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace ssc.repository
+{
+    public class US3mtsapprovalrepo
+    {
+        private string connectionString;
+
+        public US3mtsapprovalrepo(IConfiguration configuration)
+        {
+            connectionString = configuration.GetValue<string>("DBInfo:ConnectionString");
+
+        }
+        public string get_approval(int user_id)
+        {
+
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("[sscpost].[getus3mtsdata]", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@user_id", user_id);
+                    con.Open();
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    sda.Fill(dt);
+                    con.Close();
+                }
+            }
+            var asfd = dt.Rows[0];
+            //var zsdf= JsonConvert.SerializeObject(dt.Rows);
+            //var szad= JsonConvert.SerializeObject(Convert.ToString(dt.Rows[0]));
+            return JsonConvert.SerializeObject(dt);
+        }
+    }
+}
