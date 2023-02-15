40efe2da1e7099db12e82f34397a271c3da54db6
jhaabhishek8076 <jhaabhishek8076@gmail.com>
2023-02-15 12:53:35 +0530

    abhishek

---
+++ Controllers/US2userdept.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ssc.Controllers
+{
+    public class US2userdept : Controller
+    {
+        public IActionResult us2details()
+        {
+            return View();
+        }
+    }
+}
