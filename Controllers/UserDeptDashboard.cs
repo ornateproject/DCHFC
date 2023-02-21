4e7a36fcf2675d2670ef1357e6dc353145d9ae8e
jhaabhishek8076 <jhaabhishek8076@gmail.com>
2023-02-21 14:54:23 +0530

    abhishek

---
+++ Controllers/UserDeptDashboard.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ssc.Controllers
+{
+    public class UserDeptDashboard : Controller
+    {
+        public IActionResult UserDeptDash()
+        {
+            return View();
+        }
+    }
+}
