9457f4f4fda02fc96f26cfba1466a5321c4e59ec
jhaabhishek8076 <jhaabhishek8076@gmail.com>
2023-02-27 13:37:15 +0530

    abhishek

---
+++ Controllers/Dataupload_deptreg.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ssc.Controllers
+{
+    public class Dataupload_deptreg : Controller
+    {
+        public IActionResult uploadDeptdata()
+        {
+            return View();
+        }
+    }
+}
