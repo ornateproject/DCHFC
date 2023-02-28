cf07e63ec35e47b88153f0c6289ceb74552c592d
jhaabhishek8076 <jhaabhishek8076@gmail.com>
2023-02-28 16:08:09 +0530

    abhishek

---
+++ Controllers/Candidateregdata.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ssc.Controllers
+{
+    public class Candidateregdata : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
