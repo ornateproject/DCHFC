7fca93446d7d0e8edfe09d38b206a1c0d80d113b
ronyhooke <deveshjha9546@gmail.com>
2023-02-28 16:01:25 +0530

    devesh

---
+++ Controllers/Candidatemtsdata.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ssc.Controllers
+{
+    public class Candidatemtsdata : Controller
+    {
+        public IActionResult candidatemts()
+        {
+            return View();
+        }
+    }
+}
