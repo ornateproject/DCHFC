bb0ae31988b782b79e40b92fec05dffb9317b049
ronyhooke <deveshjha9546@gmail.com>
2023-03-13 15:28:01 +0530

    devesh

---
+++ Controllers/Regsuccess.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ssc.Controllers
+{
+    public class Regsuccess : Controller
+    {
+        public IActionResult regsuccess()
+        {
+            return View();
+        }
+    }
+}
