5aaac81767eec6be4421e891d92dfc908717dd2b
Neha <Neha@LAPTOP-LD47TCI5>
2023-03-15 10:18:53 +0530

    neha commit

---
+++ Controllers/DeptRegDashboardController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ssc.Models;
+using ssc.repository;
+
+namespace ssc.Controllers
+{
+    public class DeptRegDashboardController : Controller
+    {
+
+        private readonly DeptDashrepo _usdashrepo;
+        public DeptRegDashboardController(IConfiguration configuration)
+        {
+            _usdashrepo = new DeptDashrepo(configuration);
+        }
+
+        [HttpGet]
+        public IActionResult UserDeptDash()
+        {
+            var usdata = _usdashrepo.get_listdata();
+            List<DepartmentData> dept = new List<DepartmentData>();
+            dept = JsonConvert.DeserializeObject<List<DepartmentData>>(usdata);
+            return View(dept);
+        }
+        
+
+    }
+}
