c0f72be52139d26e23791f8f251c8213450240b7
Neha <Neha@LAPTOP-LD47TCI5>
2023-03-15 10:24:46 +0530

    sdfsdf

--- Controllers/DeptRegDashboardController.cs
+++ Controllers/DeptRegDashboardController.cs
@@ -5,13 +5,14 @@ using ssc.repository;
 
 namespace ssc.Controllers
 {
+    public class DeptRegDashboardController : Controller
     {
 
         private readonly DeptDashrepo _usdashrepo;
+        public DeptRegDashboardController(IConfiguration configuration)
         {
             _usdashrepo = new DeptDashrepo(configuration);
         }
-
         [HttpGet]
         public IActionResult UserDeptDash()
         {
