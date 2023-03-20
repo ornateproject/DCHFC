d1903feabce8170aaf3adcfc0376f63a31c63a08
Neha <Neha@LAPTOP-LD47TCI5>
2023-03-20 12:46:53 +0530

    neha

--- Controllers/CandidatedataController.cs
+++ Controllers/CandidatedataController.cs
@@ -8,13 +8,26 @@ namespace ssc.Controllers
 {
     public class CandidatedataController : Controller
     {
+        private readonly candidatedata_repo _candidaterepo;
+        public CandidatedataController(IConfiguration configuration)
+        {
+            _candidaterepo = new candidatedata_repo(configuration);
+           // _usdashrepo = new DeptDashrepo(configuration);
+
+
+        }
 
 
         [HttpGet]
         public IActionResult candidate()
         {
-            return View();
+            var usdata = _candidaterepo.get_postdata();
+            List<getpost> dept = new List<getpost>();
+            dept = JsonConvert.DeserializeObject<List<getpost>>(usdata);
+            return View(dept);
         }
+
+      
         public IActionResult getcandidatedata()
         {
             return View();
---
+++ repository/candidatedata_repo.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ssc.repository
+{
+    public class candidatedata_repo
+    {
+          private string connectionString;
+
+            public candidatedata_repo(IConfiguration configuration)
+            {
+                connectionString = configuration.GetValue<string>("DBInfo:ConnectionString");
+
+            }
+            public string get_postdata()
+            {
+
+                DataTable dt = new DataTable();
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("[sscpost].[Getpost_data]", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        con.Open();
+                        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                        sda.Fill(dt);
+                        con.Close();
+
+                    }
+                }
+                return JsonConvert.SerializeObject(dt);
+
+            }
+        
+    }
+}
