f957c7aa51e5c5cc8cf4013d7ec42374925c57ec
Neha <Neha@LAPTOP-LD47TCI5>
2023-03-24 17:42:29 +0530

    neha

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -122,13 +122,13 @@ namespace ssc.Controllers
         public ActionResult Approve(int id)
         {
             _login.UpdatecandidateStatus(id, "Approved");
-            return RedirectToAction("candidate", "Candidatedata");
+            return RedirectToAction("usview", "Candidatedata");
         }
 
         public ActionResult Reject(int id)
         {
             _login.UpdatecandidateStatus(id, "Rejected");
-            return RedirectToAction("candidate", "Candidatedata");
+            return RedirectToAction("usview", "Candidatedata");
         }
 
         [HttpGet]
